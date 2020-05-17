07796758d8fde82fc35747b920c2495de33e6588
RemiL'Ail <lacoquille33@gmail.com>
2020-05-17 20:06:33 +0200

    arbre

--- arbre.py
+++ arbre.py
@@ -4,8 +4,6 @@ class Arbre(pygame.sprite.Sprite) :
     def __init__(self, game) :
         super().__init__()
         self.image = pygame.image.load("assets/arbre.png")
-        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.scale(self.image, (200, 200))
         self.rect = self.image.get_rect()
         self.game = game
-        self.rect.x = self.game.map.rect.x + 1500
-        self.rect.y = self.game.map.rect.y + 1500
