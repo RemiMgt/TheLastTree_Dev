173f9aa765bb7bdd4a9e46690d2e4a4e919d9d0c
RémiMgt <lacoquille33@gmail.com>
2020-05-30 00:16:43 +0200

    encore test

--- TheLastTree/arbre.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Arbre(pygame.sprite.Sprite) :
-    def __init__(self, game) :
-        super().__init__()
-        self.image = pygame.image.load("assets/arbre.png")
-        self.image = pygame.transform.scale(self.image, (200, 200))
-        self.rect = self.image.get_rect()
-        self.game = game
